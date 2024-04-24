ee175ab9bb5c750a31cb0b24aca747147781e708
Son Nguyen Hoang <hoangson091104@gmail.com>
2024-04-23 23:28:40 -0400

    Fix: Updated documentation (#3)

--- StickyNotes-Backend/backend_django/settings.py
+++ StickyNotes-Backend/backend_django/settings.py
@@ -19,14 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1&6von56(%*jnyd5&ug$+wwtrioksw!&b-%i&8aw=b*=ucmb^!'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
